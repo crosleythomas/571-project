605022c43e7c4917b4c1be848cc6e70e0696d705
Karan Singh <karanbirrsingh@gmail.com>
2016-11-16 12:09:53 -0800

    comments, added two sample data sets, and requirements file from env

--- code/genframes.py
+++ code/genframes.py
@@ -2,32 +2,32 @@ import scipy.io as sio
 import numpy as np
 import random
 
+# This file generates a file 'video.mat' with two values:
+#	'alist' -> the image matrix, shaped (num_frames, width, height)
+#	'actions' -> a char array with the actions
+# len(actions) = len(alist) - 1 because the actions go between frames
+
+random.seed(10)
+
 sprite = sio.loadmat('../data/8x8_sprite.mat')['sprite']
 sprite_width = sprite.shape[0]
 sprite_height = sprite.shape[1]
 
-
 field_size = 1
 side_length = sprite_width * (field_size * 2 + 1)
-#field = np.zeros((side_length, side_length),dtype=np.float64)
 iter_length = 10
-result = np.zeros((side_length, side_length, iter_length), dtype=np.float64)
-
+result = np.zeros((iter_length, side_length, side_length), dtype=np.float64)
 
 # initially in center
 x = sprite_width * field_size
 y = sprite_height * field_size
-result[y:y+sprite_height, x:x+sprite_width, 0] = sprite
-
-# sio.savemat("test.mat", {'alist' : field })
+result[0, y:y+sprite_height, x:x+sprite_width] = sprite
 
 possible_actions = ['u', 'l', 'd', 'r', 'n']
 
 old_y = y
 old_x = x
 
-
-#sio.savemat("num0" + ".mat", {'alist' : field })
 choices = []
 
 for i in range(1, iter_length):
@@ -41,17 +41,12 @@ for i in range(1, iter_length):
 	elif choice == 'r': # right
 		x += sprite_width
 	if x >= 0 and x < side_length and y >= 0 and y < side_length:
-		# field[old_y:old_y+sprite_height, old_x:old_x+sprite_width] = 0
-		# field[y:y+sprite_height, x:x+sprite_width] = sprite
-		# result[i, old_y:old_y+sprite_height, old_x:old_x+sprite_width] = 0
-			
 		old_y = y
 		old_x = x
 	else:
 		x = old_x
 		y = old_y
-	result[y:y+sprite_height, x:x+sprite_width, i] = sprite	
+	result[i, y:y+sprite_height, x:x+sprite_width] = sprite	
 	choices.append(choice)
-	#sio.savemat("num" + str(i) + "action-" + choice + ".mat", {'alist' : field, 'action' : choice })
 
 sio.savemat("video.mat", {'alist' : result, 'actions' : choices })
--- code/partitionframes.py
+++ code/partitionframes.py
@@ -2,27 +2,29 @@ import scipy.io as sio
 import numpy as np
 import random
 
+# This file splits up 'video.mat' into training & label sets
+# We output 'training.mat' with the following variables
+#	'train' -> the training dataset, shaped (num_samples, window_size, width, height)
+#	'actions' -> a char array with the actions
+#	'labels' -> the labels, shaped (num_samples, width, height)
+# in this case, len(actions) = len(actions)
+
 input_matr = sio.loadmat('video.mat')
 video = input_matr['alist']
 actions = input_matr['actions']
-side_length = video.shape[0]
-iter_length = video.shape[2]
+side_length = video.shape[2]
+iter_length = video.shape[0]
 
 num_train = 3
 samples = iter_length - num_train
 
-train_frames = np.zeros((side_length, side_length, num_train), dtype = np.float64)
-test_frame = np.zeros((side_length, side_length), dtype = np.float64)
-
-training_results = np.zeros((side_length, side_length, num_train, samples), dtype = np.float64)
-test_results = np.zeros((side_length, side_length, samples), dtype = np.float64)
+training_results = np.zeros((samples, num_train, side_length, side_length), dtype = np.float64)
+test_results = np.zeros((samples, side_length, side_length), dtype = np.float64)
 action_results = []
 
 for i in range(0, iter_length - num_train):
 	action_results.append(actions[i + num_train - 1])
-	train_frames[:, :, :] = video[:, :, i:i+num_train]
-	test_frame = video[:, :, i + num_train]
-	training_results[:, :, :, i] = train_frames
-	test_results[:, :, i] = test_frame
+	training_results[i, :, :, :] = video[i:i+num_train, :, :]
+	test_results[i, :, :] = video[i + num_train, :, :]
 
 sio.savemat("training.mat", {'train' : training_results, 'actions' : action_results, 'labels' : test_results })
